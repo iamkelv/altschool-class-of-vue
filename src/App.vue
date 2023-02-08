862dc5125f77072e1573bcd9454aa1e6ed3188a6
Kelvin Moses <moseskelvin4u@gmail.com>
2023-02-08 12:47:04 +0100

    second assignment

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <script>
-import ColorPickerGame from "./components/ColorPickerGame.vue";
-import Recap_01_02_2023 from "./components/Recap_01_02_2023.vue";
+import ColorPickerGame from './components/ColorPickerGame.vue'
+import Recap_01_02_2023 from './components/Recap_01_02_2023.vue'
 
 export default {
   components: { ColorPickerGame, Recap_01_02_2023 },
   data() {
     return {
-      currentLesson: "color-picker-game",
-    };
+      currentLesson: 'color-picker-game',
+    }
   },
-};
+}
 </script>
 
 <template>
-  <ColorPickerGame v-if="currentLesson === 'color-picker-game'" />
-  <Recap_01_02_2023 v-else />
+  <div class="container">
+    <ColorPickerGame v-if="currentLesson === 'color-picker-game'" />
+    <Recap_01_02_2023 v-else />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  background: red;
+  padding: 5rem;
+  background: teal;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 10px;
+  margin: 0 auto;
+  width: auto;
+}
+</style>
--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -1,36 +1,67 @@
 <template>
-  <h1>Color Picker Game</h1>
-
-  <div>{{ message }}</div>
-
-  <button v-for="color in colors" :key="color" @click="matchColor(color)">
-    {{ color }}
-  </button>
+  <div>
+    <h1>Color Picker Game</h1>
+    <div class="body" :style="{ 'background-color': background }">
+      <div v-if="res === 'Pick a color...'">{{ res }}</div>
+      <div v-else>{{ res }}😊</div>
+    </div>
+    <div class="buttons">
+      <button v-for="color in colors" :key="color" @click="match(color)">
+        {{ color }}
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, reactive } from "@vue/reactivity";
-
+import { matchColor } from '../composables/useColorPicker'
+import { ref } from 'vue'
 export default {
   setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
-
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
-
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    };
-
-    return { colors, message, matchColor };
+    const colors = ['green', 'red', 'blue', 'purple']
+    let res = ref('Pick a color...')
+    let background = ref('')
+    const match = (color) => {
+      let { message, messageColor } = matchColor(color, colors)
+      background.value = messageColor
+      res.value = message
+    }
+    return { res, match, colors, background }
   },
-};
+}
 </script>
 
-<style></style>
+<style scoped>
+.body {
+  display: flex;
+  width: 400px;
+  justify-content: center;
+  align-items: center;
+  min-height: 60px;
+  color: white;
+  background-color: grey;
+  border-radius: 20px;
+  margin: 10px;
+}
+h1 {
+  text-align: center;
+  color: white;
+}
+
+.buttons {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0.2rem;
+}
+button {
+  background: white;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 10px;
+  padding: 1rem;
+  border: none;
+  font-weight: bold;
+  font-family: lato;
+  text-transform: capitalize;
+  cursor: pointer;
+}
+</style>
